0f4c2790eabefbbbe7358647bc3eb63d40b45fa5
MaxDovgal <max.dovgal.sever@gmail.com>
2021-10-22 16:29:10 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 "use strict";
 
-let nunber = 4.6;
+let numberOfFiles = prompt("how many films do you watch", );
+console.log(numberOfFiles);
 
-console.log(4/0);
-console.log('4/0'*9);
+// let nunber = 4.6;
 
-const persone = '5';
+// console.log(4/0);
+// console.log('4/0'*9);
+
+// const persone = '5';
